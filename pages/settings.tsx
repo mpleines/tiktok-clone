a49046e6f5af7c395e229808cbdd32885b027393
Maik Pleines <maik.pleines@googlemail.com>
2022-08-20 17:19:13 +0200

    add settings page

---
+++ pages/settings.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from 'react';
+import Page from '../components/Page/Page';
+
+interface SettingsProps {}
+
+const Settings: FunctionComponent<SettingsProps> = () => {
+  return (
+    <Page>
+      <span>Settings</span>
+    </Page>
+  );
+};
+
+export default Settings;
