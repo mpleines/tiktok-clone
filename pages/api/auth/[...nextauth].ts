4f8de2f5fe26a7b4d1d6a7b3d43cafb62e7c1009
Maik Pleines <maik.pleines@googlemail.com>
2022-08-28 15:21:31 +0200

    persist user in db

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,10 @@
+import { PrismaClient } from '@prisma/client';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+
+const createRandomUserName = () => {
+  return `conhulio-${Math.random()}`;
+};
 
 export default NextAuth({
   providers: [
@@ -8,4 +13,57 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    signIn: async ({ user }) => {
+      if (user.email == null) {
+        return false;
+      }
+
+      const prismaClient = new PrismaClient();
+
+      let hasExistingUser = false;
+
+      try {
+        const existingUser = await prismaClient.user.findUnique({
+          where: { email: user.email },
+        });
+
+        hasExistingUser = existingUser != null;
+      } catch (error) {
+        console.error(error);
+      }
+
+      if (hasExistingUser) {
+        return true;
+      }
+
+      const response = await prismaClient.user.create({
+        data: {
+          name: user.name,
+          email: user.email,
+          username: createRandomUserName(),
+        },
+      });
+
+      return response.email === user.email;
+    },
+    async session({ session }) {
+      if (session.user == null) {
+        return session;
+      }
+
+      const prismaClient = new PrismaClient();
+      const existingUser = await prismaClient.user.findUnique({
+        where: { email: session.user.email },
+      });
+
+      if (existingUser?.username == null) {
+        return session;
+      }
+
+      session.user.username = existingUser?.username;
+
+      return session;
+    },
+  },
 });
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,18 +1,35 @@
 import { signOut, useSession } from 'next-auth/react';
 import { FunctionComponent } from 'react';
 import Button from '../components/Button/Button';
+import Input from '../components/Input/Input';
 import Page from '../components/Page/Page';
 
 interface SettingsProps {}
 
 const Settings: FunctionComponent<SettingsProps> = () => {
   const session = useSession();
+  const user = session?.data?.user;
 
   return (
     <Page sessionRequired>
-      <h2>Settings</h2>
-      <span>Logged in as {session.data?.user?.email}</span>
-      <Button title="Logout" onClick={signOut} />
+      <h2>Profile</h2>
+
+      <Input
+        label="Username"
+        value={user.username}
+        onChange={() => null}
+        type="text"
+      />
+      <Input
+        label="Full Name"
+        value={user.name}
+        onChange={() => null}
+        type="text"
+      />
+      <div>
+        <Button title="Save" onClick={() => null} />
+        <Button title="Logout" onClick={signOut} />
+      </div>
     </Page>
   );
 };
