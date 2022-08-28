951a36f13414c4b58a09b645b42e240e53b203d6
Maik Pleines <maik.pleines@googlemail.com>
2022-08-28 17:02:24 +0200

    add avatar from google acount

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -1,3 +1,4 @@
+import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { FunctionComponent } from 'react';
 import styles from './Avatar.module.css';
@@ -8,10 +9,15 @@ interface AvatarProps {
 }
 
 const Avatar: FunctionComponent<AvatarProps> = ({ imgSrc = Placeholder }) => {
+  const session = useSession();
+  const { image } = session.data?.user;
+
+  const img = session.status === 'loading' ? undefined : image ? image : imgSrc;
+
   return (
     <Image
       alt="User Avatar"
-      src={imgSrc}
+      src={img}
       className={styles.wrapper}
       width="32"
       height="32"
