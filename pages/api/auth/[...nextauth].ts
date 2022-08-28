43ca7525a81f0b1a0e23eecfa9a4b5a2b2808696
Maik Pleines <maik.pleines@googlemail.com>
2022-08-28 16:52:23 +0200

    improve creating random user name

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,8 +2,44 @@ import { PrismaClient } from '@prisma/client';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+const userNameChars = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+const userNameNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const createRandomUserName = () => {
-  return `conhulio-${Math.random()}`;
+  return `${Array.from(Array(6).keys())
+    .map(() => userNameChars[Math.floor(Math.random() * userNameChars.length)])
+    .join('')}${Array.from(Array(4).keys())
+    .map(
+      () => userNameNumbers[Math.floor(Math.random() * userNameNumbers.length)]
+    )
+    .join('')}`;
 };
 
 export default NextAuth({
