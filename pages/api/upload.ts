a7a1377e83fd40f721c2fbbd87891e1f1951fb77
Maik Pleines <maik.pleines@googlemail.com>
2022-12-02 22:08:23 +0100

    temp

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
+import B2 from 'backblaze-b2';
 
 const prisma = new PrismaClient();
 
@@ -11,6 +12,20 @@ export default async function handler(
 
   try {
     const response = await prisma.post.create({ data: post });
+
+    // upload video to backblaze
+    // FIXME: do not push this! move to .env files
+    const backblaze = new B2({
+      applicationKeyId: '004896f07f8979a0000000001',
+      applicationKey: 'K004hFiR+epD5TLkRGJXwUUOFBNiTDQ',
+    });
+
+    const authResponse = await backblaze.authorize();
+
+    backblaze.uploadFile({
+      
+    })
+
 
     res.status(200).send({ post });
   } catch (error) {
