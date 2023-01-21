1c5979057e13a4726bbd4df943df69254cbb56df
Maik Pleines <maik.pleines@googlemail.com>
2023-01-21 15:13:20 +0100

    update schema and play video inline

--- components/Camera/Camera.tsx
+++ components/Camera/Camera.tsx
@@ -1,5 +1,5 @@
-import { FunctionComponent, useEffect, useRef, useState } from "react";
-import Button from "../Button/Button";
+import { FunctionComponent, useEffect, useRef, useState } from 'react';
+import Button from '../Button/Button';
 import styles from './Camera.module.css';
 
 const mediaOptions = {
@@ -10,10 +10,14 @@ const mediaOptions = {
 interface CameraProps {
   showCamera: boolean;
   onCancel: () => void;
-  handleChange: (blob: Blob) => void; 
+  handleChange: (blob: Blob) => void;
 }
 
-const Camera: FunctionComponent<CameraProps> = ({ showCamera, onCancel, handleChange }) => {
+const Camera: FunctionComponent<CameraProps> = ({
+  showCamera,
+  onCancel,
+  handleChange,
+}) => {
   const preview = useRef<HTMLVideoElement>(null);
   const mediaStream = useRef<MediaStream>();
   const recorder = useRef<MediaRecorder>();
@@ -26,7 +30,7 @@ const Camera: FunctionComponent<CameraProps> = ({ showCamera, onCancel, handleCh
 
     navigator.mediaDevices.getUserMedia(mediaOptions).then((stream) => {
       mediaStream.current = stream;
-      
+
       if (preview.current == null) {
         return;
       }
@@ -37,23 +41,25 @@ const Camera: FunctionComponent<CameraProps> = ({ showCamera, onCancel, handleCh
 
   const startRecording = () => {
     if (mediaStream.current == null) {
-      console.error("MediaStream is null")
+      console.error('MediaStream is null');
       return;
     }
 
-    recorder.current = new MediaRecorder(mediaStream.current, { mimeType: 'video/webm' });
+    recorder.current = new MediaRecorder(mediaStream.current, {
+      mimeType: 'video/webm',
+    });
     recorder.current.start();
 
     recorder.current.ondataavailable = (event) => {
       handleChange(event.data);
-    }
+    };
 
     setIsRecording(true);
-  }
+  };
 
   const stopRecording = () => {
     if (recorder.current == null) {
-      console.error('recorder is null')
+      console.error('recorder is null');
       return;
     }
 
@@ -61,35 +67,38 @@ const Camera: FunctionComponent<CameraProps> = ({ showCamera, onCancel, handleCh
 
     setIsRecording(false);
     closeCameraStopMediaStream();
-  }
+  };
 
   const closeCameraStopMediaStream = () => {
     if (mediaStream.current == null) {
-      return; 
+      return;
     }
 
-    mediaStream.current.getTracks().forEach(track => track.stop());
+    mediaStream.current.getTracks().forEach((track) => track.stop());
     onCancel();
-  }
+  };
 
   return (
     <div className={styles.wrapper}>
       <video
         ref={preview}
-        className={styles.camera} 
+        className={styles.camera}
         autoPlay
-        muted/>
+        muted
+        playsInline
+        controls={false}
+      />
       <div className={styles.actions}>
         <Button
-          onClick={() => isRecording ? stopRecording() : startRecording()}
+          onClick={() => (isRecording ? stopRecording() : startRecording())}
           title={isRecording ? 'Stop' : 'Start'}
         />
       </div>
       <div className={styles.topActions}>
-        <Button onClick={closeCameraStopMediaStream} title="Close"/>
+        <Button onClick={closeCameraStopMediaStream} title="Close" />
       </div>
     </div>
   );
-}
+};
 
 export default Camera;
